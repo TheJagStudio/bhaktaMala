ddab0b4c728f576008a6b2f672a8a54f6e9d3c23
Jagrat Patel <81472856+TheJagStudio@users.noreply.github.com>
2023-11-30 16:16:09 +0530

    Add files via upload

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -13,6 +13,30 @@ const Card = ({ name, father, address, phone, pradesh, photo, activeP }) => {
         }
     }
 
+    function dataURItoBlob(dataURI) {
+        // convert base64 to raw binary data held in a string
+        // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+        var byteString = atob(dataURI.split(",")[1]);
+
+        // separate out the mime component
+        var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+
+        // write the bytes of the string to an ArrayBuffer
+        var ab = new ArrayBuffer(byteString.length);
+
+        // create a view into the buffer
+        var ia = new Uint8Array(ab);
+
+        // set the bytes of the buffer to the correct values
+        for (var i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+
+        // write the ArrayBuffer to a blob, and you're done
+        var blob = new Blob([ab], { type: mimeString });
+        return blob;
+    }
+
     return (
         <div
             onClick={() => {
@@ -28,7 +52,8 @@ const Card = ({ name, father, address, phone, pradesh, photo, activeP }) => {
 
                             toPng(card, { cacheBust: false })
                                 .then((dataUrl) => {
-                                    let files = [new File([dataUrl], "bhaktamala.png", { type: "image/png" })];
+                                    let blob = dataURItoBlob(dataUrl);
+                                    let files = [new File([blob], name + ".png", { type: "image/png" })];
                                     shareData({
                                         title: "Bhaktamala",
                                         text: "Name : " + name + "\nFather : " + father + "\nPhone : " + phone + "\nAddress : " + address + "\nPradesh : " + pradesh,
